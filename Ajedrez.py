40ccba0c7051b7d6b47eee0875162f63af322588
Cathe28 <catherinegenes@gmail.com>
2024-04-29 18:52:42 -0400

    clase2

--- Ajedrez.py
+++ Ajedrez.py
@@ -62,6 +62,14 @@ white_pawn = pygame.transform.scale(white_pawn, (65, 65))
 white_pawn_small = pygame.transform.scale(white_pawn, (45, 45))
 
 
+white_images = [white_pawn,white_queen,white_king,white_knight,white_rook,white_bishop]
+white_images = [white_pawn_small,white_queen_small,white_king_small,white_knight_small,white_rook_small,white_bishop_small]
 
+black_images = [black_pawn,black_queen,black_king,black_knight,black_rook,black_bishop]
+black_images = [black_pawn_small,black_queen_small,black_king_small,black_knight_small,black_rook_small,black_bishop_small]
+piece_list = ['pawn', 'queen', 'king', 'knight', 'rook', 'bishop']
 
+counter = 0
+winner = ''
+game_over = False
 
